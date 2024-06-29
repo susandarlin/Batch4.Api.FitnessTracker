6c02bd282e80b70df8439147ffc5f2499cbf8937
Sandar <susandarlin.2016@gmail.com>
2024-06-29 16:55:26 +0700

    Add DataAccess for User

---
+++ Batch4.Api.FitnessTracker.DataAccess/Db/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Batch4.Api.FitnessTracker.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch4.Api.FitnessTracker.DataAccess.Db;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionString.builder.ConnectionString);
+    }
+
+    public DbSet<User> Users { get; set; }
+}
