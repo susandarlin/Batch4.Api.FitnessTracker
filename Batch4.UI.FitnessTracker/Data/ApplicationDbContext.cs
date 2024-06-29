8deb954d8527cca60f74db856d4e0f8a40abb1a1
Sandar <susandarlin.2016@gmail.com>
2024-06-29 16:22:01 +0700

    Add DataAccess and BusinessLogic projects

---
+++ Batch4.UI.FitnessTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Batch4.UI.FitnessTracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Batch4.UI.FitnessTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Batch4.UI.FitnessTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
