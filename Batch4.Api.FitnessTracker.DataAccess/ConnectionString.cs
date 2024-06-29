c76b7a296a2af90bada4c1c8450ab0f615ccb605
Sandar <susandarlin.2016@gmail.com>
2024-06-29 16:55:26 +0700

    Add DataAccess for User

---
+++ Batch4.Api.FitnessTracker.DataAccess/ConnectionString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch4.Api.FitnessTracker.DataAccess;
+
+public static class ConnectionString
+{
+    public static SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+    {
+        DataSource = "SANDAR\\MSSQLSERVER2012",
+        InitialCatalog = "SSDLDotNetCore",
+        UserID = "sa",
+        Password = "admin123!",
+        TrustServerCertificate = true
+    };
+}
---
+++ Batch4.Api.FitnessTracker.DataAccess/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch4.Api.FitnessTracker.DataAccess.Models;
+
+[Table("Tbl_User")]
+public class User
+{
+    
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Password { get; set; }
+}
